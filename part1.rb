63b7367531355b844029da315dff3a879898d860
Guilcher Pascal <pgu.dev@gmail.com>
2012-08-02 23:08:36 +0200

    ..

--- part1.rb
+++ part1.rb
@@ -1,14 +1,49 @@
+# a
 def palindrome?(string)
  string.gsub(/\W/, "").downcase == string.gsub(/\W/, "").downcase.reverse
 end
 
-def testPalindrome(string, expected)
+def test_palindrome?(string, expected)
+  puts "----- test of palindrome function -----"
   result = palindrome?(string)
   puts "[#{string}] #{expected}? #{result}" 
+  puts
 end
 
-testPalindrome("A man, a plan, a canal -- Panama", true)
-testPalindrome("Madam, I'm Adam!", true)
-testPalindrome("Abracadabra", false)
+test_palindrome?("A man, a plan, a canal -- Panama", true)
+test_palindrome?("Madam, I'm Adam!", true)
+test_palindrome?("Abracadabra", false)
+
+# b
+
+def count_words(string)
+  h = Hash.new(0)
+  a = string.downcase.split(/\b\W+\b/i)
+  a.each { |i| h[i] += 1 }
+  return h
+end
+
+
+
+def test_count_words()
+  puts "----- test of count words function -----"
+  text = "A man, a plan, a canal -- Panama"
+  expected = {'a' => 3, 'man' => 1, 'canal' => 1, 'panama' => 1, 'plan' => 1}
+  actual = count_words(text)
+  puts "[#{text}]? #{actual == expected}" 
+  puts actual.inspect
+
+  text = "Doo bee doo bee doo"
+  expected = {'doo' => 3, 'bee' => 2}
+  actual = count_words(text)
+  puts "[#{text}]? #{actual == expected}" 
+  puts actual.inspect
+
+  puts
+end
+
+test_count_words
+
+
 
 
